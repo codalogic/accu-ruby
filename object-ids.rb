175a51f1daed9accebe45073b3380739dae87140
Pete Cordell <pete.git@codalogic.com>
2017-06-17 09:22:57 +0100

    Removed object-ids.rb experiment

--- object-ids.rb
+++
@@ -1,7 +0,0 @@
-puts "1000.object_id = #{3.object_id}"
-a = 1000
-puts "a = #{a}, a.object_id = #{a.object_id}"
-b = a
-puts "b = #{b}, b.object_id = #{b.object_id}"
-a += 1
-puts "a = #{a}, a.object_id = #{a.object_id}"
