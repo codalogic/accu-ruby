e4e20ba93b1d58eeea2ea129c69548f4cb31434a
Pete Cordell <pete.git@codalogic.com>
2017-06-20 12:43:06 +0100

    Snippets updated

--- snippets.rb
+++ snippets.rb
@@ -5,7 +5,7 @@ my_hash = { "author" => "Pete", "reader" => "you" }
 my_range = 0..10
 
 my_array.each { |x| print "_#{x}" }
-puts  # Put new line at end of above line
+puts  # Put new line at end of above
 my_hash.each do |role, name|
   if role == "reader"
     puts "#{name}, Thanks for reading"
@@ -13,17 +13,17 @@ my_hash.each do |role, name|
 end
 
 10.times { |x| print "_#{x}" }
-puts  # Put new line at end of above line
+puts  # Put new line at end of above
 (0..9).each { |x| print "_#{x}" }
-puts  # Put new line at end of above line
+puts  # Put new line at end of above
 0.upto(9) { |x| print "_#{x}" }
-puts  # Put new line at end of above line
+puts  # Put new line at end of above
 
 puts (0..100).map { |x| x * x }
          .select { |x| x.odd? }
          .reduce { |acc,x| acc + x }
 
-class Array; alias :filter :select; end
+class Array; alias filter select; end
 
 puts (0..100).map{ |x| x * x }
          .filter{ |x| x.odd? }
