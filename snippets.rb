3bab4ec821d3d4cee2d42530aac55bf1caa1b406
Pete Cordell <pete.git@codalogic.com>
2017-06-20 11:59:37 +0100

    Added types and iterators to snippets.rb

--- snippets.rb
+++ snippets.rb
@@ -1,1 +1,35 @@
 puts "Hello, World!"
+
+my_array = [ 1, 2, 3, "Four", 5 ]
+my_hash = { "author" => "Pete", "reader" => "you" }
+my_range = 0..10
+
+my_array.each { |x| print "_#{x}" }
+puts  # Put new line at end of above line
+my_hash.each do |role, name|
+  if role == "reader"
+    puts "#{name}, Thanks for reading"
+  end
+end
+
+10.times { |x| print "_#{x}" }
+puts  # Put new line at end of above line
+(0..9).each { |x| print "_#{x}" }
+puts  # Put new line at end of above line
+0.upto(9) { |x| print "_#{x}" }
+puts  # Put new line at end of above line
+
+puts (0..100).map { |x| x * x }
+         .select { |x| x.odd? }
+         .reduce { |acc,x| acc + x }
+
+class Array; alias :filter :select; end
+
+puts (0..100).map{ |x| x * x }
+         .filter{ |x| x.odd? }
+         .reduce{ |acc,x| acc + x }
+
+numbers = [1,2,3,4]
+squares = numbers.map {|x| x*x}
+
+puts "Numbers:", numbers, "Squares:", squares
